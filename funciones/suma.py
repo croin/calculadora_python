acf038bc0c28f47a94215b8e4c24f8a822d367e7
croin <andresgonzalez.sistemas2012@gmail.com>
2017-11-26 19:50:01 -0500

    agregando la funcion suma

---
+++ funciones/suma.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+def suma():
+	num1 = int(raw_input("digite número 1"))
+	num2 = int(raw_input("digite número 2"))
+	
+	suma = num1 + num2
+	
+	print "la suma de los 2 numes es",suma
